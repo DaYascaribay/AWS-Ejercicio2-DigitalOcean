2a96d85d07bce5fc6461964f9048af3ad973ee5d
DaYascaribay <david-ldu03@hotmail.com>
2025-05-06 18:50:59 -0500

    mejora de carpetas

--- backend/app.py
+++ backend/app.py
@@ -28,4 +28,4 @@ def load_user():
     return jsonify({'result':datos})
 
 if __name__=="__main__":
-    app.run(debug=True, host="0.0.0.0", port="666")
+    app.run(debug=True, host="0.0.0.0", port="8081")
