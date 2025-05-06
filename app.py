73c13614183fb931ab68064919eb449380acbead
DaYascaribay <david-ldu03@hotmail.com>
2025-05-06 17:27:57 -0500

    pequeña modificacion en el puerto

--- app.py
+++ app.py
@@ -5,24 +5,24 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     datos = [
-        {'id':1,
-         'name':'Vrouqen',
-         'description':'Me gusta programar'
-         },
-         {'id':2,
-         'name':'Nico',
-         'description':'Hola'
-         },
-         {'id':3,
-         'name':'Bryan',
-         'description':'Hola como estás'
-         },
-         {'id':4,
-         'name':'Derek',
-         'description':'No confíen en mí'
-         }
+            {'id':1,
+            'name':'Vrouqen',
+            'description':'Me gusta programar'
+            },
+            {'id':2,
+            'name':'Nico',
+            'description':'Hola'
+            },
+            {'id':3,
+            'name':'Bryan',
+            'description':'Hola como estás'
+            },
+            {'id':4,
+            'name':'Derek',
+            'description':'No confíen en mí'
+            }
     ]
     return render_template("index.html", datos=datos)
 
 if __name__ == "__main__":
-    app.run(debug = True, host = "0.0.0.0", port = "555")
+    app.run(debug = True, host = "0.0.0.0", port = 555)
