6449c3069d3598f7f30853803e144a261e83fa41
DaYascaribay <david-ldu03@hotmail.com>
2025-05-06 18:58:35 -0500

    mejora de archivos

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask("__name__")
 
@@ -7,4 +8,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug = True, host = "0.0.0.0", port = "8080")
+    port = int(os.getenv("PORT", 8080))  # Usa 8080 por defecto si no se establece una variable de entorno
+    app.run(debug=True, host="0.0.0.0", port=port)
